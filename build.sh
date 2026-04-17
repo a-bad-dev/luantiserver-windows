266f2838402d5e976c9a2e12f58ddf21c799e89f
a-bad-dev <244852891+a-bad-dev@users.noreply.github.com>
2026-04-17 00:32:14 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,18 @@
 
 VERSION="5.15.2"
 
-# system update
+# system update (skip this most of the time)
 pacman -Syu
 
 # install deps
 pacman -S zip git mingw-w64-clang-x86_64-{clang,cmake,ninja,curl-winssl,libpng,libjpeg-turbo,freetype,libogg,libvorbis,sqlite3,openal,zstd,gettext,luajit,SDL2}
 
-# prepare to compile
+# download and extract sources
 curl -Lo luanti.tar.gz https://github.com/luanti-org/luanti/archive/refs/tags/${VERSION}.tar.gz
+
 gunzip luanti.tar.gz
 tar -xf luanti.tar
+
 cd luanti-${VERSION}/
 
 # configure
@@ -24,14 +26,16 @@ cmake . -G Ninja -DRUN_IN_PLACE=TRUE -DBUILD_CLIENT=FALSE -DBUILD_SERVER=TRUE
 ninja -j$(nproc)
 
 # bundle DLLs
-cp ../bundle_dlls.sh util/
-chmod +x util/bundle_dlls.sh
+chmod +x ../bundle_dlls.sh
 
-./util/bundle_dlls.sh bin/luantiserver.exe bin/
+../bundle_dlls.sh bin/luantiserver.exe bin/
 
 # build the zip archive of luantiserver
-mkdir luantiserver-${VERSION}-msys2-win64
-cp -r bin/ builtin/ games/ mods/ textures/ worlds/ luantiserver-${VERSION}-msys2-win64/
+mkdir -p luantiserver-${VERSION}-msys2-win64/textures/
+
+cp -r bin/ builtin/ games/ mods/ worlds/ luantiserver-${VERSION}-msys2-win64/
+cp textures/texture_packs_here.txt luantiserver-${VERSION}-msys2-win64/textures/
+
 zip -r9 luantiserver-${VERSION}-msys2-win64.zip luantiserver-${VERSION}-msys2-win64/
 
 # clean up
